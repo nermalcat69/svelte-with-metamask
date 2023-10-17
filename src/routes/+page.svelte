22dd141cbd4e5370687355693f7aa51db8923c14
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-10-17 22:31:23 +0100

    tailwind setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../app.css';
+</script>
+
+<h1 class="text-5xl font-bold text-gray-700 text-center pt-12">Welcome to SvelteKit</h1>
+<p class="text-center pt-4 font-bold text-gray-600">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
